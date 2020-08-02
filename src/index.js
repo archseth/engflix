f5026dbbcd17d0d079bb0bd8dbcf1d2e58408be2
brenocolaco <brenocolaco@gmail.com>
2020-08-02 14:39:32 -0300

    Setup do Projeto

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import App from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+// Desafio!!!
+const Pagina404 = () => (<div>Página 404</div>);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+
+    </Switch>
+
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
